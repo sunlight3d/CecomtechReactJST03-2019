6f6f0f9495640451d8533ae1f48de81b936129fb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-02-16 19:32:05 +0700

    update

--- Exercise01/userDetail.php
+++ Exercise01/userDetail.php
@@ -1,12 +1,13 @@
 <?php 
 error_reporting(0);
 $firstname = $_GET["firstname"];
+
 $lastname = $_GET["lastname"];
 $address = $_GET["address"];
 $contactNo = $_GET["contactNo"];
 echo "<h2>Your personal details:</h2>";
 echo "Firstname: $firstname<br>";
 echo "Lastname: $lastname<br>";
-echo "Address: $address<br>";
+echo "<h1>Address: ".$address."</h1>";
 echo "Contact No: $contactNo<br>";
 ?>
