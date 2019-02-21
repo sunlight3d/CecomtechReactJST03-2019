358efae9d85fa845e16ee7cfef47c545c056b134
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-02-21 19:46:10 +0700

    update

--- bai04/index.php
+++ bai04/index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <h1>Registration form</h1>
-<form method="POST">
+<form method="POST" action="includes/register.inc.php">
     UserName:<input type="text" name="UserName">
     <br>
     Password:<input type="password" name="Password">
@@ -12,14 +12,24 @@
     <button type="submit" name="submit">Register User</button>
 </form>
 <?php
-include_once('./database/database.inc.php');
-include_once('./database/controllers.inc.php');
-
-if(isset($_POST['submit'])) {
-    echo "<h2>Bam dang ky</h2>";
-    //$connection;
-    registerUser();
-}
+if (isset($_GET['register_result'])) {
+        $register_result = $_GET['register_result'];
+        switch ($register_result) {
+            case 'empty':
+                echo "You must enter input information";
+                break;
+            case 'success':
+                echo "Register success";
+                break;
+            case 'error':
+                echo "Error inserting to DB";
+                break;
+            default:
+                echo "Default:";
+                break;
+        }
+    }
+    
 ?>
 
 </body>
