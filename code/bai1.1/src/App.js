80a17dbef3733b94e35da448e44e5e858324af63
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-03-14 13:00:45 +0700

    update

---
+++ code/bai1.1/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <div>
+            <h1>Hello world</h1>
+        </div>
+    }
+}
+export default App
