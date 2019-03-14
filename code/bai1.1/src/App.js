52c7c1c6bae79b1faf18d32c633950e3d93881f5
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-03-14 22:18:16 +0700

    update

--- code/bai1.1/src/App.js
+++ code/bai1.1/src/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './App.css'
+import {doSomething, multiply, testLocalStorage, doPromiseA} from './Utility'
 
 class App extends Component {
     constructor(props) {
         super(props)
     }
     render() {
+        //doSomething()
+        doPromiseA(1,2).then((result) =>{
+            alert(`result = ${result}`)
+        }).catch((err) => {
+            alert(`Error = ${err}`)
+        })
+        // testLocalStorage()
+        // multiply(3,10)
         return <div>
             <h1>Hello world</h1>
         </div>
---
+++ code/bai1.1/src/Utility.js
@@ -0,0 +1,58 @@
+
+const doSomething = () => {
+    var x = 10
+    if(x == '10') {
+        // alert('It is 10')
+    }
+    //Compare 
+    if(x === 10) {
+        // alert('It is true')
+    }
+    //undefined and not defined
+    var a;
+    //alert(`a = ${a}`)
+    // if(a === undefined) {}
+    // alert(`a2 = ${a2}`)
+    if(1) {
+        let x = 11
+    }
+    alert(`x = ${x}`)
+    let name="Hoang"
+    let email = "hoang@gmail.com"
+    // alert(`Name = ${name}, email = ${email}`)    
+    //Object
+    var person = {name: "Hoang", email: "hoang@gmail.com"}
+    //key can be number, Vietnamese string
+    //person.name.length    
+    //alert(`person = ${JSON.stringify(person)}`)
+    // let {name, email} = person
+    //alert(`Name = ${name}, email = ${email}`)    
+    //Delete an object
+    // delete person
+
+}
+function multiply(x, y) {
+    alert(`x . y = ${x * y}`)        
+    return x * y;
+}
+//local storage
+const testLocalStorage = () => {
+    // localStorage.setItem('name', 'Hoang')
+    alert(`Name = ${localStorage.getItem('name')}`)
+}
+//Promise function 
+const doPromiseA = (param1, param2) => {
+    return new Promise((resolve, reject) => {
+        if(param1 == 1 && param2 == 2) {
+            resolve("Successful")
+        } else {
+            reject("Failed")
+        }
+    })
+}
+export {
+    doSomething,
+    multiply,
+    testLocalStorage,
+    doPromiseA
+}
